a4a179ff48d3499cd7854a835f48104e278cd6e2
Loc Le <lpl212757@gmail.com>
2024-07-31 19:31:42 +0700

    feat - update mapping

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,3 +8,29 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+-- n, v, i, t = mode names
+
+-- Lspconfig Code action
+map("i", "<C-y>", function() vim.lsp.buf.code_action() end , { desc = "lsp code_action", })
+
+-- User config
+-- LSPConfig
+map("n", "gD", function() vim.lsp.buf.declaration() end, { desc = "lsp declaration", noremap=true, silent=true })
+map("n", "gd", function() vim.lsp.buf.definition() end, { desc = "lsp definition", noremap = true, silent = true })
+map("n", "K", function() vim.lsp.buf.hover() end, { desc = "lsp hover", })
+map("n", "gi", function() require("telescope.builtin").lsp_implementations() end, { desc = "lsp implementation", noremap = true, silent = true })
+map("n", "gr", function() require("telescope.builtin").lsp_references() end, { desc = "lsp references", noremap = true, silent = true})
+map("n", "go", function() require("telescope.builtin").lsp_document_symbols() end, { desc = "lsp document symbols", noremap = true, silent = true})
+map("n", "<C-y>", function() vim.lsp.buf.code_action() end, { desc = "lsp code_action", })
+map("n", "<F2>", function() vim.lsp.buf.rename() end, { desc = "lsp rename", })
+map("n", "<leader>fm", function() vim.lsp.buf.format { async = true } end, { desc = "LSP formatting", })
+map("v", "<leader>fm", function() vim.lsp.buf.format { async = true } end, { desc = "lsp formatting", })
+map("n", "[d", function() vim.diagnostic.goto_prev() end, { desc = "goto prev" })
+map("n", "]d", function() vim.diagnostic.goto_next() end, { desc = "goto_next" })
+map("n", "<leader>q", function() vim.diagnostic.setloclist() end, { desc = "diagnostic setloclist", })
+map("n", "<leader>f", function() vim.diagnostic.open_float() end, { desc = "floating diagnostic", })
+
+map("n", "<leader>q", function() vim.diagnostic.setloclist() end, { desc = "Diagnostic setloclist", })
+map("n", "<C-p>", "<cmd> Telescope find_files <CR>", { desc = "find files" })
+map("n", "<leader>fp", "<cmd> Telescope projects <CR>", { desc = "Project" })
+-- End user config
