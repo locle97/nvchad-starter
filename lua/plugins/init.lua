7105f3895bfc217886c1c0baccce670ea5f322eb
Loc Le <lpl212757@gmail.com>
2024-07-31 19:32:02 +0700

    feat - add some plugins

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,5 +1,71 @@
 return {
   {
+    "https://git.sr.ht/~swaits/zellij-nav.nvim",
+    lazy = true,
+    event = "VeryLazy",
+    keys = {
+      { "<c-h>", "<cmd>ZellijNavigateLeft<cr>",  { silent = true, desc = "navigate left"  } },
+      { "<c-j>", "<cmd>ZellijNavigateDown<cr>",  { silent = true, desc = "navigate down"  } },
+      { "<c-k>", "<cmd>ZellijNavigateUp<cr>",    { silent = true, desc = "navigate up"    } },
+      { "<c-l>", "<cmd>ZellijNavigateRight<cr>", { silent = true, desc = "navigate right" } },
+    },
+    opts = {},
+  },
+  {
+    "zbirenbaum/copilot-cmp",
+    event = { "InsertEnter", "LspAttach" },
+    fix_pairs = true,
+    config = function()
+      require("copilot_cmp").setup()
+    end,
+  },
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      require("copilot").setup {
+        suggestion = { enabled = false },
+        panel = { enabled = false },
+      }
+    end,
+  },
+    { "neo4j-contrib/cypher-vim-syntax", event = "BufEnter" },
+  {
+    "echasnovski/mini.nvim",
+    event = "BufEnter",
+    config = function()
+      require("mini.cursorword").setup {}
+    end,
+  },
+
+  {
+    "f-person/git-blame.nvim",
+    event = "BufEnter",
+  },
+
+  {
+    "ahmedkhalf/project.nvim",
+    lazy = false,
+    config = function()
+      require("project_nvim").setup {}
+    end,
+  },
+
+    {
+    "akinsho/toggleterm.nvim",
+    lazy = false,
+    config = function()
+      require("toggleterm").setup {
+        open_mapping = [[<c-\>]],
+        direction = "float",
+        shade_terminals = true,
+      }
+    end,
+  },
+
+  { "kdheepak/lazygit.nvim", lazy = false },
+  {
     "stevearc/conform.nvim",
     -- event = 'BufWritePre', -- uncomment for format on save
     opts = require "configs.conform",
