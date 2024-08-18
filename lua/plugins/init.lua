a0f413211e68545b4cd43877811acfdf7e11b4a4
Loc Le <lpl212757@gmail.com>
2024-08-18 18:53:56 +0700

    feat - move to using tmux instead of zellij

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -101,18 +101,6 @@ return {
     end,
   },
   {
-    "https://git.sr.ht/~swaits/zellij-nav.nvim",
-    lazy = true,
-    event = "VeryLazy",
-    keys = {
-      { "<c-h>", "<cmd>ZellijNavigateLeft<cr>",  { silent = true, desc = "navigate left" } },
-      { "<c-j>", "<cmd>ZellijNavigateDown<cr>",  { silent = true, desc = "navigate down" } },
-      { "<c-k>", "<cmd>ZellijNavigateUp<cr>",    { silent = true, desc = "navigate up" } },
-      { "<c-l>", "<cmd>ZellijNavigateRight<cr>", { silent = true, desc = "navigate right" } },
-    },
-    opts = {},
-  },
-  {
     "zbirenbaum/copilot-cmp",
     event = { "InsertEnter", "LspAttach" },
     fix_pairs = true,
