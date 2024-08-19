052e14ff7999877707518d1df7eeaf345971cd92
Loc Le <lpl212757@gmail.com>
2024-08-19 12:21:48 +0700

    feat - vim dash board

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,4 +1,4 @@
--- This file needs to have same structure as nvconfig.lua 
+-- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v2.5/lua/nvconfig.lua
 
 ---@type ChadrcConfig
@@ -8,10 +8,49 @@ M.base46 = {
 	theme = "catppuccin",
   transparency = true,
 
-	-- hl_override = {
+
+  -- hl_override = {
   Comment = { italic = true },
   ["@comment"] = { italic = true },
-	-- },
+  -- },
 }
+
+M.ui = {
+  nvdash = {
+    load_on_startup = true,
+
+    header = {
+      "                   -`                   ",
+      "                  .o+`                  ",
+      "                 `ooo/                  ",
+      "                `+oooo:                 ",
+      "               `+oooooo:                ",
+      "               -+oooooo+:               ",
+      "             `/:-:++oooo+:              ",
+      "            `/++++/+++++++:             ",
+      "           `/++++++++++++++:            ",
+      "          `/+++ooooooooooooo/`          ",
+      "         ./ooosssso++osssssso+`         ",
+      "        .oossssso-````/ossssss+`        ",
+      "       -osssssso.      :ssssssso.       ",
+      "      :osssssss/        osssso+++.      ",
+      "     /ossssssss/        +ssssooo/-      ",
+      "   `/ossssso+/:-        -:/+osssso+-    ",
+      "  `+sso+:-`                 `.-/+oso:   ",
+      " `++:.                           `-/+/  ",
+      " .`                                 `   ",
+    },
+
+    buttons = {
+      { "  Find File", "Spc f f", "Telescope find_files" },
+      { "󰈚  Recent Files", "Spc f o", "Telescope oldfiles" },
+      { "󰈭  Find Word", "Spc f w", "Telescope live_grep" },
+      { "  Bookmarks", "Spc m a", "Telescope marks" },
+      { "  Themes", "Spc t h", "Telescope themes" },
+      { "  Mappings", "Spc c h", "NvCheatsheet" },
+    },
+  },
+}
+
 
 return M
