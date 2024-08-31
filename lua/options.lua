5837ebd8192ac45a18435486e5c01f16ed024d95
Loc Le <lpl212757@gmail.com>
2024-08-31 09:08:51 +0700

    feat - update tabstop to 4

--- lua/options.lua
+++ lua/options.lua
@@ -2,13 +2,18 @@ require "nvchad.options"
 
 -- add yours here!
 
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+local o = vim.o
 local g = vim.g
 local opt = vim.opt
 
 g.gitblame_enabled = false
 g.gitblame_date_format = '%r'
 g.gitblame_highlight_group = 'GitBlameMessage'
+
+-- Indenting
+o.expandtab = true
+o.shiftwidth = 4
+o.smartindent = true
+o.tabstop = 4
 
 opt.relativenumber = true
