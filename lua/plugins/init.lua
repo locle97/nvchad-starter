714f7742ba05319a35de83124297db599c0bfeba
Loc Le <lpl212757@gmail.com>
2024-08-31 09:10:19 +0700

    Format

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,180 +1,180 @@
 return {
-  {
-    "nvim-tree/nvim-tree.lua",
-    cmd = { "NvimTreeToggle", "NvimTreeFocus" },
-    opts = {
-      filters = {
-        dotfiles = false,
-        exclude = { vim.fn.stdpath "config" .. "/lua/custom" },
-      },
-      disable_netrw = true,
-      hijack_netrw = true,
-      hijack_cursor = true,
-      hijack_unnamed_buffer_when_opening = false,
-      sync_root_with_cwd = false,
-      update_cwd = false,
-      update_focused_file = {
-        enable = true,
-        update_root = false,
-      },
-      view = {
-        centralize_selection = false,
-        cursorline = true,
-        debounce_delay = 15,
-        side = "left",
-        preserve_window_proportions = false,
-        number = false,
-        relativenumber = false,
-        signcolumn = "yes",
-        width = 30,
-        float = {
-          enable = true,
-          quit_on_focus_loss = true,
-          open_win_config = {
-            relative = "editor",
-            border = "rounded",
-            width = 75,
-            height = 50,
-            row = 1,
-            col = 1,
-          },
-        },
-      },
-      git = {
-        enable = true,
-        ignore = true,
-      },
-      filesystem_watchers = {
-        enable = true,
-      },
-      actions = {
-        open_file = {
-          resize_window = true,
-        },
-      },
-      renderer = {
-        root_folder_label = false,
-        highlight_git = true,
-        highlight_opened_files = "none",
-
-        indent_markers = {
-          enable = false,
-        },
-
-        icons = {
-          show = {
-            file = true,
-            folder = true,
-            folder_arrow = true,
-            git = true,
-          },
-
-          glyphs = {
-            default = "󰈚",
-            symlink = "",
-            folder = {
-              default = "",
-              empty = "",
-              empty_open = "",
-              open = "",
-              symlink = "",
-              symlink_open = "",
-              arrow_open = "",
-              arrow_closed = "",
+    {
+        "nvim-tree/nvim-tree.lua",
+        cmd = { "NvimTreeToggle", "NvimTreeFocus" },
+        opts = {
+            filters = {
+                dotfiles = false,
+                exclude = { vim.fn.stdpath "config" .. "/lua/custom" },
+            },
+            disable_netrw = true,
+            hijack_netrw = true,
+            hijack_cursor = true,
+            hijack_unnamed_buffer_when_opening = false,
+            sync_root_with_cwd = false,
+            update_cwd = false,
+            update_focused_file = {
+                enable = true,
+                update_root = false,
+            },
+            view = {
+                centralize_selection = false,
+                cursorline = true,
+                debounce_delay = 15,
+                side = "left",
+                preserve_window_proportions = false,
+                number = false,
+                relativenumber = false,
+                signcolumn = "yes",
+                width = 30,
+                float = {
+                    enable = true,
+                    quit_on_focus_loss = true,
+                    open_win_config = {
+                        relative = "editor",
+                        border = "rounded",
+                        width = 75,
+                        height = 50,
+                        row = 1,
+                        col = 1,
+                    },
+                },
             },
             git = {
-              unstaged = "✗",
-              staged = "✓",
-              unmerged = "",
-              renamed = "➜",
-              untracked = "★",
-              deleted = "",
-              ignored = "◌",
+                enable = true,
+                ignore = true,
             },
-          },
+            filesystem_watchers = {
+                enable = true,
+            },
+            actions = {
+                open_file = {
+                    resize_window = true,
+                },
+            },
+            renderer = {
+                root_folder_label = false,
+                highlight_git = true,
+                highlight_opened_files = "none",
+
+                indent_markers = {
+                    enable = false,
+                },
+
+                icons = {
+                    show = {
+                        file = true,
+                        folder = true,
+                        folder_arrow = true,
+                        git = true,
+                    },
+
+                    glyphs = {
+                        default = "󰈚",
+                        symlink = "",
+                        folder = {
+                            default = "",
+                            empty = "",
+                            empty_open = "",
+                            open = "",
+                            symlink = "",
+                            symlink_open = "",
+                            arrow_open = "",
+                            arrow_closed = "",
+                        },
+                        git = {
+                            unstaged = "✗",
+                            staged = "✓",
+                            unmerged = "",
+                            renamed = "➜",
+                            untracked = "★",
+                            deleted = "",
+                            ignored = "◌",
+                        },
+                    },
+                },
+            },
         },
-      },
+        config = function(_, opts)
+            dofile(vim.g.base46_cache .. "nvimtree")
+            require("nvim-tree").setup(opts)
+        end,
     },
-    config = function(_, opts)
-      dofile(vim.g.base46_cache .. "nvimtree")
-      require("nvim-tree").setup(opts)
-    end,
-  },
-  {
-    "zbirenbaum/copilot-cmp",
-    event = { "InsertEnter", "LspAttach" },
-    fix_pairs = true,
-    config = function()
-      require("copilot_cmp").setup()
-    end,
-  },
-  {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    event = "InsertEnter",
-    config = function()
-      require("copilot").setup {
-        suggestion = { enabled = false },
-        panel = { enabled = false },
-      }
-    end,
-  },
-  { "neo4j-contrib/cypher-vim-syntax", event = "BufEnter" },
-  {
-    "echasnovski/mini.nvim",
-    event = "BufEnter",
-    config = function()
-      require("mini.cursorword").setup {}
-    end,
-  },
+    {
+        "zbirenbaum/copilot-cmp",
+        event = { "InsertEnter", "LspAttach" },
+        fix_pairs = true,
+        config = function()
+            require("copilot_cmp").setup()
+        end,
+    },
+    {
+        "zbirenbaum/copilot.lua",
+        cmd = "Copilot",
+        event = "InsertEnter",
+        config = function()
+            require("copilot").setup {
+                suggestion = { enabled = false },
+                panel = { enabled = false },
+            }
+        end,
+    },
+    { "neo4j-contrib/cypher-vim-syntax", event = "BufEnter" },
+    {
+        "echasnovski/mini.nvim",
+        event = "BufEnter",
+        config = function()
+            require("mini.cursorword").setup {}
+        end,
+    },
 
-  {
-    "f-person/git-blame.nvim",
-    event = "BufEnter",
-  },
+    {
+        "f-person/git-blame.nvim",
+        event = "BufEnter",
+    },
 
-  {
-    "ahmedkhalf/project.nvim",
-    lazy = false,
-    config = function()
-      require("project_nvim").setup {}
-    end,
-  },
+    {
+        "ahmedkhalf/project.nvim",
+        lazy = false,
+        config = function()
+            require("project_nvim").setup {}
+        end,
+    },
 
-  {
-    "akinsho/toggleterm.nvim",
-    lazy = false,
-    config = function()
-      require("toggleterm").setup {
-        open_mapping = [[<c-\>]],
-        direction = "float",
-        shade_terminals = true,
-      }
-    end,
-  },
+    {
+        "akinsho/toggleterm.nvim",
+        lazy = false,
+        config = function()
+            require("toggleterm").setup {
+                open_mapping = [[<c-\>]],
+                direction = "float",
+                shade_terminals = true,
+            }
+        end,
+    },
 
-  { "kdheepak/lazygit.nvim",           lazy = false },
-  {
-    "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
-    opts = require "configs.conform",
-  },
+    { "kdheepak/lazygit.nvim",           lazy = false },
+    {
+        "stevearc/conform.nvim",
+        -- event = 'BufWritePre', -- uncomment for format on save
+        opts = require "configs.conform",
+    },
 
-  -- These are some examples, uncomment them if you want to see them work!
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require "configs.lspconfig"
-    end,
-  },
+    -- These are some examples, uncomment them if you want to see them work!
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            require "configs.lspconfig"
+        end,
+    },
 
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+    -- {
+    -- 	"nvim-treesitter/nvim-treesitter",
+    -- 	opts = {
+    -- 		ensure_installed = {
+    -- 			"vim", "lua", "vimdoc",
+    --      "html", "css"
+    -- 		},
+    -- 	},
+    -- },
 }
