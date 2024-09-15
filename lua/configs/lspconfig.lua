b5672545c6b83fe0c3a196f6f168dd938b921098
Loc Le <lpl212757@gmail.com>
2024-09-15 21:58:56 +0700

    feat - add Python

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls", "omnisharp", "jsonls" }
+local servers = { "html", "cssls", "omnisharp", "jsonls", "pylsp" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
